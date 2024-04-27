1b657ce401850efcc4b92549694b5ec02cacf985
BrenonOrtega <brenon.ortega@hotmail.com>
2024-04-27 19:02:31 +0200

    Readds backend.tf without keys

--- infrastructure/backend.tf
+++ infrastructure/backend.tf
@@ -1,8 +1,4 @@
 # Define Terraform backend using a S3 bucket for storing the Terraform state
 terraform {
-  backend "s3" {
-    bucket = "infrastructure-state"
-    key =  "${var.environment}-state/tofu.tfstate"
-    region = "${var.region}"
- }
+  backend "s3" {}
 }
