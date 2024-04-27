239103c579f3af4aeb4411684a605c360ae892cd
BrenonOrtega <brenon.ortega@hotmail.com>
2024-04-27 19:49:19 +0200

    Removes local aws tf provider info

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -8,13 +8,7 @@ terraform {
 }
 
 provider "aws" {
-  region = "eu-south-2"
-  profile = "real-billable-aws"
-  shared_config_files      = ["C:\\Users\\Bryan\\.aws\\config"]
-  shared_credentials_files = ["C:\\Users\\Bryan\\.aws\\credentials"]
 }
-
-
 
 module "application" {
     source = "./modules/application"
